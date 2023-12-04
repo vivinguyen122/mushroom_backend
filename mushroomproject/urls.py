565e81440c425848b1baa687de7523ecfbd3deac
Vivi <vivinguyen@cmail.carleton.ca>
2023-12-04 16:50:59 -0500

    update urls

--- mushroomproject/urls.py
+++ mushroomproject/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from mushroomapp import urls, views
 
 urlpatterns = [
 
     path('admin', admin.site.urls),
-    path('', views.index, name="index")
+    path('', include('mushroomapp.urls'))
 
 ]
